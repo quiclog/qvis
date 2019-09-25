941a8d1251c536e61cb0be822de61ab6227bf1ec
Robin Marx <marx.robin@gmail.com>
2019-09-25 08:56:23 -0700

    Add legends to the congestion graph

--- visualizations/src/components/congestiongraph/CongestionGraphRenderer.vue
+++ visualizations/src/components/congestiongraph/CongestionGraphRenderer.vue
@@ -1,7 +1,57 @@
 <template>
     <div>
-        <div id="congestion-graph" style="width: 100%; border:5px solid #d4edda; min-height: 200px;">
+        <div id="congestion-graph" style="width: 100%; border:5px solid #d4edda; min-height: 200px; position: relative;">
+            <div id="legendPackets" style="padding: 5px; border: 1px solid black; background-color: white; position: absolute; top: 10px; left: 80px; z-index: 100;" @mouseenter="hideLegend" @mouseleave="showLegend">
+                    
+                <p style="padding: 0; margin: 10px 0;"> 
+                    <span style="width: 12px; height: 8px; background-color: blue; display: inline-block; margin-right: 1px; vertical-align: middle;"></span> 
+                    : PACKET_SENT
+                </p>
+                <p style="padding: 0; margin: 10px 0;"> 
+                    <span style="width: 12px; height: 8px; background-color: #6B8E23; display: inline-block; margin-right: 1px; vertical-align: middle;"></span> 
+                    : PACKET_ACKED
+                </p>
+                <p style="padding: 0; margin: 10px 0;"> 
+                    <span style="width: 12px; height: 8px; background-color: red; display: inline-block; margin-right: 1px; vertical-align: middle;"></span> 
+                    : PACKET_LOST
+                </p>
+
+            </div>
+
+            <div id="legendMetrics" style="padding: 5px; border: 1px solid black; background-color: white; position: absolute; top: 440px; right: 50px; z-index: 100;" @mouseenter="hideLegend" @mouseleave="showLegend">
+            
+                <p style="padding: 0; margin: 10px 0;"> 
+                    <span style="width: 12px; height: 8px; background-color: #8A2BE2; display: inline-block; margin-right: 1px; vertical-align: middle;"></span> 
+                    : Congestion window
+                </p>
+                <p style="padding: 0; margin: 10px 0;"> 
+                    <span style="width: 12px; height: 8px; background-color: #808000; display: inline-block; margin-right: 1px; vertical-align: middle;"></span> 
+                    : Bytes in flight
+                </p>
+
+            </div>
+
+
+            <div id="legendRTT" style="padding: 5px; border: 1px solid black; background-color: white; position: absolute; top: 795px; right: 50px; z-index: 100;" @mouseenter="hideLegend" @mouseleave="showLegend">
+                
+                <p style="padding: 0; margin: 3px 15px; display: inline;"> 
+                    <span style="width: 32px; height: 8px; background-color: #C96480; display: inline-block; margin-right: 1px; vertical-align: middle;"></span>
+                    : Min RTT
+                </p>
+                
+                <p style="padding: 0; margin: 3px 15px; display: inline;"> 
+                    <span style="width: 32px; height: 8px; background-color: #ff9900; display: inline-block; margin-right: 1px; vertical-align: middle;"></span>
+                    : Latest RTT
+                </p>
+                <p style="padding: 0; margin: 3px 15px; display: inline;"> 
+                    <span style="width: 32px; height: 8px; background-color: #8a554a; display: inline-block; margin-right: 1px; vertical-align: middle;"></span>
+                    : Smoothed RTT
+                </p>
+            </div>
+
         </div>
+
+        
     </div>
 </template>
 
@@ -13,6 +63,11 @@
     .nogrid line {
         stroke: #ffffff;
     }
+
+    .hiddenLegend {
+        opacity: 0.3;
+    }
+
 </style>
 
 <script lang="ts">
@@ -41,6 +96,16 @@
             if ( this.config && this.renderer && this.config.connection !== undefined ) {
                 this.renderer.render( this.config );
             }
+        }
+
+        protected hideLegend(evt:any) {
+            console.log("Hiding legend ", evt);
+            evt.target.classList.add("hiddenLegend");
+        }
+
+        protected showLegend(evt:any) {
+            console.log("Showing legend ", evt);
+            evt.target.classList.remove("hiddenLegend");
         }
 
         // Note: we could use .beforeUpdate or use an explicit event or a computed property as well
